7dc0cbe6be0f063570dd4553cd75d369a85de331
Raghu Reddy <raghureddy.ram@gmail.com>
2019-02-04 15:49:41 -0800

    Regiser book in admin and add help text

--- books/models.py
+++ books/models.py
@@ -5,7 +5,11 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    author = models.CharField(max_length=50)
+    author = models.CharField(max_length=50, help_text="Use pen name, not real name")
     review = models.TextField(blank=True, null=True)
     date_reviewed = models.DateTimeField(blank=True, null=True)
-    is_favorite = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False, verbose_name="Favorite?")
+
+
+    def __str__(self):
+        return self.title
